b4583246b7b4d5a7b0f15edb457fc86ef3927758
josh-ip <joshua.ip@yale.edu>
2019-07-12 18:22:07 +0900

    push

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,14 +1,17 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-//#import "GoogleMaps/GoogleMaps.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  //[GMSServices provideAPIKey:@"AIzaSyDXDB3KCbtJ0SDS8sc5hE5N7pSroQ2Rbg0"];
+  [GMSServices provideAPIKey:@"AIzaSyDXDB3KCbtJ0SDS8sc5hE5N7pSroQ2Rbg0"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  //FirebaseApp.configure();
+  //FIRApp.configure();
+
 }
 
 @end
